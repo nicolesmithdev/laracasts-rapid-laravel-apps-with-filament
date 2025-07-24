9b4858af69474ebec7e701ba3655e62b03548f42
Nicole Smith <nicolesmith@mac.lan>
2025-07-24 08:12:50 -0400

    S2 (Filament Forms): Checkbox List

--- app/Filament/Resources/ConferenceResource.php
+++ app/Filament/Resources/ConferenceResource.php
@@ -6,6 +6,7 @@ use App\Enums\Region;
 use App\Filament\Resources\ConferenceResource\Pages;
 use App\Filament\Resources\ConferenceResource\RelationManagers;
 use App\Models\Conference;
+use App\Models\Speaker;
 use App\Models\Venue;
 use Filament\Forms;
 use Filament\Forms\Form;
@@ -61,6 +62,10 @@ class ConferenceResource extends Resource
                     ->relationship('venue', 'name', modifyQueryUsing: function(Builder $query, Forms\Get $get) {
                         return $query->where('region', $get('region'));
                     }),
+                Forms\Components\CheckboxList::make('speakers')
+                    ->relationship('speakers', 'name')
+                    ->options(Speaker::pluck('name', 'id'))
+                    ->required(),
             ]);
     }
 
--- app/Models/Speaker.php
+++ app/Models/Speaker.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -19,11 +22,52 @@ class Speaker extends Model
     {
         return [
             'id' => 'integer',
+            'qualifications' => 'array'
         ];
     }
 
     public function conferences(): BelongsToMany
     {
         return $this->belongsToMany(Conference::class);
+    }
+
+    public static function getForm(): array
+    {
+        return [
+            TextInput::make('name')
+                ->required()
+                ->maxLength(255),
+            TextInput::make('email')
+                ->email()
+                ->required()
+                ->maxLength(255),
+            Textarea::make('bio')
+                ->required()
+                ->columnSpanFull(),
+            TextInput::make('twitter_handle')
+                ->required()
+                ->maxLength(255),
+            CheckboxList::make('qualifications')
+                ->columnSpanFull()
+                ->searchable()
+                ->bulkToggleable()
+                ->options([
+                    'business-leader' => 'Business Leader',
+                    'charisma' => 'Charisma',
+                    'first-time' => 'First Time Speaker',
+                    'hometown-hero' => 'Hometown Hero',
+                    'humanitarian' => 'Humanitarian',
+                    'laracasts-contributor' => 'Laracasts Contributor',
+                    'twitter-influencer' => 'Large Twitter Following',
+                    'youtube-influencer' => 'Large YouTube Following',
+                    'open-source' => 'Open Source Creator / Maintainer',
+                    'unique-perspective' => 'Unique Perspective'
+                ])
+                ->descriptions([
+                    'business-leader' => 'Here is a nice long description',
+                    'charisma' => 'This is even more information about you should pick this one'
+                ])
+                ->columns(3)
+        ];
     }
 }
